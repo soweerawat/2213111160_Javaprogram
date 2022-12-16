918ed9c4e71eb8db6f0192d4deeaa4c792c2b772
soweerawat <ph.weerawat_st@tni.ac.th>
2022-12-16 15:07:56 +0700

    Attendance[15-12-2022]

--- JavaProgram/src/System/out/Lab606.java
+++ JavaProgram/src/System/out/Lab606.java
@@ -1,22 +1,38 @@
 package System.out;
-import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class Lab606 {
-
+	static int number[] = { 25, 78, 41, 22, 36, 85,37};
 	public static void main(String[] args) {
-		int number[] = { 25, 78, 41, 22, 36, 85,37};
 		int _index = Integer.parseInt(JOptionPane.showInputDialog("Input index of array: "));
-	    while() {
-	    	
-	    }
+		 checkIndex(number,_index);
+	 while(!checkIndex(number,_index)) {
+	 _index = Integer.parseInt(JOptionPane.showInputDialog("Input index of array,again: "));
+	 }
+	JOptionPane.showMessageDialog(null,"Current data,nums["+_index+"] is "
+	 +currentData(number,_index)+
+	 ((_index!=0) ? "\nPrevious data,nums["+(_index-1)+"] is "
+	 +prevData(number,_index) : "\nNo previous data ")+
+	 ((_index!=6) ? "\nNext data,nums["+(_index+1)+"] is "
+	 +nextData(number,_index) : "\nNo next data "));
+	
 		
 	
+	}  
 	public static boolean checkIndex(int[] nums,int index) {
-		
+		return (index>=0 && index<=6) ? true : false ;
 	}
-	  
-
+	public static int currentData(int[] nums,int index) {
+		return nums[index];
+	}
+	public static int prevData(int[] nums,int index) {
+		return nums[index-1];
+	}
+	public static int nextData(int[] nums,int index) {
+		return nums[index+1];
+	}
+			
+}
 	
 
 
